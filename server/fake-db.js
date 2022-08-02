5d83b9616a75f2521ac675dbb022390c31f2e8d0
iWatabe1 <oneseki743@gmail.com>
2022-08-02 17:15:01 +0900

    Nodeサーバー構築

---
+++ server/fake-db.js
@@ -0,0 +1,80 @@
+const Product = require('./model/product');
+
+class FakeDb {
+    constructor() {
+        this.products = [
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                id: 1,
+                name: 'Phone XL',
+                price: 799,
+                description: 'A large phone with one of the best screens',
+                heading1: 'sample text1',
+                heading2: 'sample text3',
+                heading3: 'sample text3',
+                headingText1: 'sample text sample text sample text sample text',
+                headingText2: 'sample text sample text sample text sample text',
+                headingText3: 'sample text sample text sample text sample text'
+              },
+              {
+                coverImage: './assets/img/phone-cover.jpg',
+                id: 2,
+                name: 'Phone Mini',
+                price: 699,
+                description: 'A great phone with one of the best cameras',
+                heading1: 'sample text1',
+                heading2: 'sample text3',
+                heading3: 'sample text3',
+                headingText1: 'sample text sample text sample text sample text',
+                headingText2: 'sample text sample text sample text sample text',
+                headingText3: 'sample text sample text sample text sample text'
+              },
+              {
+                coverImage: './assets/img/phone-cover.jpg',
+                id: 3,
+                name: 'Phone Standard',
+                price: 299,
+                description: '',
+                heading1: 'sample text1',
+                heading2: 'sample text3',
+                heading3: 'sample text3',
+                headingText1: 'sample text sample text sample text sample text',
+                headingText2: 'sample text sample text sample text sample text',
+                headingText3: 'sample text sample text sample text sample text'
+              },
+              {
+                coverImage: './assets/img/phone-cover.jpg',
+                id: 4,
+                name: 'iPad air',
+                price: 599,
+                description: 'A good ipad',
+                heading1: 'sample text1',
+                heading2: 'sample text3',
+                heading3: 'sample text3',
+                headingText1: 'sample text sample text sample text sample text',
+                headingText2: 'sample text sample text sample text sample text',
+                headingText3: 'sample text sample text sample text sample text'
+              }
+        ]
+    }
+
+    async initDb() {
+      await this.clearnDb();
+      this.pushProductsToDb();
+    }
+
+    async clearnDb() {
+      await Product.deleteMany({});
+    }
+
+    pushProductsToDb() {
+        this.products.forEach(
+            (product) => {
+                const newProduct = new Product(product);
+                newProduct.save();
+            }
+        )
+    }
+}
+
+module.exports = FakeDb;
