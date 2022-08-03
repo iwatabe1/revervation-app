3119af13a8a0b51ae2392c493784c82690cf53b9
iWatabe1 <oneseki743@gmail.com>
2022-08-03 18:53:36 +0900

    authフロントエンド部分作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 
+
 const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  // { path: 'detail', component: ProductDetailComponent }
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    ProductModule
+    ProductModule,
+    AuthModule
   ],
   exports: [RouterModule]
 })
