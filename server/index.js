ef99414306068cafe0bbe66bc76b9e280f8e4e25
iWatabe1 <oneseki743@gmail.com>
2022-08-06 23:35:34 +0900

    ログイン、新規会員登録機能作成

--- server/index.js
+++ server/index.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config/dev');
-const app = express();
-const PORT = process.env.PORT || 3001;
+const config = require('./config/index');
 const FakeDb = require('./fake-db');
-const productRoutes = require('./routes/products');
 
-mongoose.connect(config.DB_URI).then(
+const productRoutes = require('./routes/products');
+const userRoutes = require('./routes/users');
+const path = require('path');
+
+mongoose.connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(
     () => {
-        const fakeDb = new FakeDb();
-        fakeDb.initDb();
+        if(process.env.NODE_ENV !== 'production') {
+            const fakeDb = new FakeDb();
+            // fakeDb.initDb();
+        }
     }
 );
 
+const app = express();
+app.use(express.json());
 app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    const appPath = path.join( __dirname, '..', 'dist', 'reservation-app');
+    app.use(express.static(appPath));
+    app.get("*", function(req, res) {
+        res.sendFile(path.resolve(appPath, 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, function() {
     console.log('I am runnning');
